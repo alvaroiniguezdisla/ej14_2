659ac15f9b20e8bfa66384d2e653a11a7fa15163
Álvaro <ainiguezdislaa@gmail.com>
2023-02-25 14:18:35 +0100

    ej terminado

--- src/dominio/ej14_2.java
+++ src/dominio/ej14_2.java
@@ -1,2 +1,11 @@
-package dominio;public class ej14_2 {
+package dominio;
+import java.util.stream.Stream;
+
+
+public class ej14_2 {
+    public static int suma(int[]lista) {
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).reduce(0,(a,b)->a+b);
+
+    }
+
 }
